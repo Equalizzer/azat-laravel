091b30aa786f8840e593f5e4c85fbcdd60c2de64
VaheHarutyunyan21 <vaheh@internet.ru>
2021-10-20 20:17:35 +0400

    init commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('admin.home');
+});
+
+use App\Http\Controllers\UserController;
+
+
+Route::post('/login',[UserController::class, 'postLogin'] );
+Route::get('/login', [UserController::class, 'getLogin']);
+
+Route::get('/sign-up', [UserController::class, 'getSignUp'])->name('user.signup');
+Route::post('/sign-up', [UserController::class, 'postSignUp']);
+
+
+//Route::post('login', 'UserControllerLogin');    //xamp 7.2 i hamar
